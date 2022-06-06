f48c9f7dda8fabf961a29edd0a1a4404289ec3c6
dribe <bekizepp@gmail.com>
2022-06-06 08:42:31 +0200

    add docs

--- leksione/07-files.php
+++ leksione/07-files.php
@@ -1,16 +1,15 @@
 <?php
 
 // fopen(filename, mode) where mode: w, r, a, x, w+, r+, a+, x+
-// fclose($file)
-// boolean feof($file)
-// char fgetc($file)
-// string fgets($file)
-// fwrite($file, $string)
-
-// include(“header.php”)
-// require(“header.php”)
-// include_once(“header.php”)
-// require_once(“header.php”)
+// fclose(filename):            closes file
+// boolean feof(filename):      checks if file has reached EOF
+// char fgetc(filename):        gets next character in file
+// string fgets(filename):      gets next word in file
+// fwrite(filename, str):       writes content to file
+// include(filename):           includes external file
+// require(filename):           includes external file
+// include_once(filename):      includes external file
+// require_once(filename):      includes external file
 
 $file1 = fopen("f1.txt", "r") or exit("Error Opening File");
 $file2 = fopen("f2.txt", "w") or exit("Error Opening File");
